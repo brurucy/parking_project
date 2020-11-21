adf36484d51f9245d1406add9a8b59775575f00d
Amirabbas Sherafatian <amirabba@ut.ee>
2020-11-21 05:45:47 +0200

    the first draft for parking controller is ready now

--- lib/parkingproject_web/controllers/parking_controller.ex
+++ lib/parkingproject_web/controllers/parking_controller.ex
@@ -1,41 +1,66 @@
-defmodule ParkingProjectWeb.parkingController do
+defmodule ParkingProjectWeb.ParkingController do
   use ParkingProjectWeb, :controller
+
   import Ecto.Query, only: [from: 2]
 
   alias ParkingProject.Repo
-  alias ParkingProject.Sales.{Taxi, parking, Allocation}
+  alias ParkingProject.ParkingSpace.{Parking}
   alias Ecto.{Changeset, Multi}
+  alias ParkingProjectWeb.Geolocation
+
 
   def index(conn, _params) do
     user = ParkingProject.Authentication.load_current_user(conn)
-    parkings = Repo.all(from b in parking, where: b.user_id == ^user.id)
+    parkings = Repo.all(from b in Parking, where: b.user_id == ^user.id)
     render conn, "index.html", parkings: parkings
   end
 
   def new(conn, _params) do
-    changeset = parking.changeset(%parking{}, %{})
+    changeset = Parking.changeset(%Parking{}, %{})
     render conn, "new.html", changeset: changeset
   end
 
   def create(conn, %{"parking" => parking_params}) do
     user = ParkingProject.Authentication.load_current_user(conn)
 
-    parking_struct = Enum.map(parking_params, fn({key, value}) -> {String.to_atom(key), value} end)
-                     |> Enum.into(%{})
-
-    IO.inspect parking_struct
-
-    changeset = %parking{}
-                |> parking.changeset(parking_struct)
-                |> Changeset.put_change(:user, user)
-                |> Changeset.put_change(:status, "taken")
+    parking_struct = Ecto.build_assoc(user, :parking, Enum.map(parking_params, fn({key, value}) -> {String.to_atom(key), value} end))
+    changeset = Parking.changeset(parking_struct, %{})
+                  |> Changeset.put_change(:status, "taken")
 
     case Repo.insert(changeset) do
-      {:ok, _} -> ?
-      nil ->
-    end
+      {:ok, _} ->
 
-    end
+        foundParkingArea = "Narva maantee 18, 51009, Tartu"
+        [dis, dur] = Geolocation.distance(parking_params.destination, foundParkingArea)
+
+        conn
+          |> put_flash(:info, "Parking confirmed, distance: #{dis}, duration: #{dur}")
+          |> redirect(to: Routes.parking_path(conn, :index))
+
+      _ ->
+
+        conn
+        |> flashTheChangeset(changeset)
+        |> render("new.html", changeset: changeset)
+      end
+
+  end
+
+  defp flashTheChangeset(conn, changeset) do
+
+    errors = Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
+
+    error_msg =
+      errors
+      |> Enum.map(fn {_key, errors} -> "#{Enum.join(errors, ", ")}" end)
+      |> Enum.join("\n")
+
+    conn
+      |> put_flash(:error, error_msg)
   end
 
 end
