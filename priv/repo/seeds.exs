8017dd36eb3e70615c62dfea6cd51134d8f76ba4
kasesalum <kasesalum@gmail.com>
2020-11-25 11:40:38 +0200

    testing

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,10 +1,22 @@
 alias ParkingProject.Repo
 alias ParkingProject.UserManagement.User
 
+alias ParkingProject.ParkingSpace.{Booking, Allocation, Parking}
+
 
 User.changeset(%User{},
         %{name: "RUcy",
           email: "bruno98@ut.ee",
           password: "parool",
           license_plate: "666SATYR"}) |> Repo.insert()
+
+
+[%{spot: "Vabriku", category: "B", places: 3, latitude: 58.37681, longitude: 26.70399},
+%{spot: "Lossi", category: "A", places: 30, latitude: 58.37927, longitude: 26.71754},
+%{spot: "Jakobi", category: "A", places: 50, latitude: 58.38237, longitude: 26.71489}]
+|> Enum.map(fn parking_data -> Parking.changeset(%Parking{}, parking_data) end)
+|> Enum.each(fn changeset -> Repo.insert!(changeset) end)
+
+
+
         
