386aef92d5f21661469f09e9a3e48efe2d1f0317
kasesalum <kasesalum@gmail.com>
2020-11-30 22:08:53 +0200

    removed unnecessary test

--- test/parkingproject_web/controllers/page_controller_test.exs
+++ test/parkingproject_web/controllers/page_controller_test.exs
@@ -1,8 +1,5 @@
 defmodule ParkingProjectWeb.PageControllerTest do
   use ParkingProjectWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
+
 end
