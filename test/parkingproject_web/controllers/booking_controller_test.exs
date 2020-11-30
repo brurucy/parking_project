27740224b6275bd0f32c94c81c6de7508d66228b
kasesalum <kasesalum@gmail.com>
2020-11-30 21:55:51 +0200

    test

--- test/parkingproject_web/controllers/booking_controller_test.exs
+++ test/parkingproject_web/controllers/booking_controller_test.exs
@@ -14,6 +14,16 @@ defmodule ParkingProjectWeb.ParkingControllerTest do
     assert html_response(conn, 200) =~ ~r/Parking confirmed/
   end
 
+  test "display parking place details", %{conn: conn} do
+    conn = post conn, "/sessions", %{session: [email: "bruno98@ut.ee", password: "parool"]}
+    conn = get conn, redirected_to(conn)
+    current_user = Repo.get_by(User, email: "bruno98@ut.ee")
+    conn = post conn, "/bookings", %{booking: [user: current_user, destination: "Raatuse 23", duration: 50.0]}
+  #  :timer.sleep(2500)
+    conn = get conn, redirected_to(conn)
+    assert html_response(conn, 200) =~ ~r/Parking confirmed1/
+  end
+
   test "closest location", %{conn: conn} do
     conn = post conn, "/sessions", %{session: [email: "bruno98@ut.ee", password: "parool"]}
     conn = get conn, redirected_to(conn)
