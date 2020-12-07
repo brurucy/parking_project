9b44976c488b59b079cfbd1502c8929faeacfa60
kasesalum <kasesalum@gmail.com>
2020-12-07 20:28:39 +0200

    test

--- test/parkingproject_web/controllers/booking_controller_test.exs
+++ test/parkingproject_web/controllers/booking_controller_test.exs
@@ -28,6 +28,7 @@ defmodule ParkingProjectWeb.ParkingControllerTest do
     }
     
     :timer.sleep(2500)
+    conn = get conn, redirected_to(conn)
     assert html_response(conn, 200) =~ ~r/No field in start date can be empty/
  
   end
