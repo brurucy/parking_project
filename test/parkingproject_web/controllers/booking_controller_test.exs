c387814c6bcfd73501b78ad7626cc3d010b82340
kasesalum <kasesalum@gmail.com>
2020-12-07 18:46:04 +0200

    test

--- test/parkingproject_web/controllers/booking_controller_test.exs
+++ test/parkingproject_web/controllers/booking_controller_test.exs
@@ -141,7 +141,7 @@ defmodule ParkingProjectWeb.ParkingControllerTest do
     current_user = Repo.get_by(User, email: "bruno98@ut.ee")
 
     IO.inspect current_user, label: "current user"
-    IO.puts "/users/" <> String.to_integer(current_user.id), label: "WHAT"
+    IO.puts "/users/" <> current_user.id, label: "WHAT"
 
     conn = put conn, "/users/" <> String.to_integer(current_user.id), %{"id" => current_user.id, "user" => %{"is_hourly" => "false"}}
     false_hourly_user = Repo.get_by(User, email: "bruno98@ut.ee")
