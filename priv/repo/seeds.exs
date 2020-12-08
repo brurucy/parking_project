3c038d2e08323a2a24703dc20f277977e1aacf1d
kasesalum <kasesalum@gmail.com>
2020-12-08 08:30:59 +0200

    test

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -20,7 +20,7 @@ fee_b = %ParkingFee{}
 
 [%{spot: "Vabriku 1", parking_fee_id: fee_a.id, places: 3, latitude: 58.37681, longitude: 26.70399},
 %{spot: "Lossi 21", parking_fee_id: fee_b.id, places: 30, latitude: 58.37927, longitude: 26.71754},
-%{spot: "Jakobi 2", parking_fee_id: fee_b.id, places: 50, latitude: 58.38237, longitude: 26.71489}
+%{spot: "Jakobi 2", parking_fee_id: fee_b.id, places: 50, latitude: 58.38237, longitude: 26.71489},
 %{spot: "Pepleri 14", parking_fee_id: fee_b.id, places: 0, latitude: 58.38238, longitude: 26.71488}]
 |> Enum.map(fn parking_data -> Parking.changeset(%Parking{}, parking_data) end)
 |> Enum.each(fn changeset -> Repo.insert!(changeset) end)
